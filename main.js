74d17b3353615774cec5c3131c1d6986b7959b36
obayomi96 <martinsoluwaseun47@gmail.com>
2018-11-26 11:48:09 +0000

    Increasing item quantity

--- main.js
+++ main.js
@@ -29,6 +29,21 @@ addToCartButtonsDOM.forEach(addToCartButtonDOM => {
 
       cart.push(product);
       addToCartButtonDOM.innerText = 'In Cart';
+      
+// Increasing quantity
+      
+    const cartItemsDOM = cartDOM.querySelectorAll('.cart__item');
+      cartItemsDOM.forEach(cartItemDOM => {
+        if (cartItemDOM.querySelector('.cart__item__name').innerText === product.name) {
+          cartItemDOM.querySelector('[data-action="INCREASE_ITEM"]').addEventListener('click', () => {
+            cart.forEach(cartItem => {
+              if (cartItem.name === product.name) {
+                cartItemDOM.querySelector('.cart__item__quantity').innerText = ++cartItem.quantity;
+              }
+            });
+          });
+        }
+      });
     }
   });
 });
